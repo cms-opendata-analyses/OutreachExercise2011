6ed673ad6f171ce60b9ec0517f2dc2032e5624e1
Ana Yaiza Rodriguez Marrero <ayrodrig@gmail.com>
2014-10-29 17:26:17 +0100

    Added License Information

--- OutreachExercise2010/DecaysToLeptons/python/Sample.py
+++ OutreachExercise2010/DecaysToLeptons/python/Sample.py
@@ -1,9 +1,21 @@
-#
-# Copyright (c) 2014 A.Y. Rodriguez-Marrero and E. Fernandez-del-Castillo
-# Based on the code of the CMSData Analysis School 2014 Long Exercise: 
-# Search for the Higgs in ZZ -> 4 leptons decay channel (available 
-# at https://github.com/bachtis/CMSDAS)
-#
+## This file is part of CMSOutreachExercise2010.
+## Copyright (C) 2014 Instituto de Fisica de Cantabria and CERN.
+## Based on the code of the CMSData Analysis School 2014 Long Exercise: 
+## Search for the Higgs in ZZ -> 4 leptons decay channel (available 
+## at https://github.com/bachtis/CMSDAS)
+
+## CMSOutreachExercise2010 is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## CMSOutreachExercise2010 is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with CMSOutreachExercise2010. If not, see <http://www.gnu.org/licenses/>.
 
 class Sample(object):
     def __init__(self, type, isMC, files, crossSection):
